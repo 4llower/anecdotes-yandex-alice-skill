eb92e3a5ca5ff81a5dad16ead6250778f5df8c23
edelwud <edelwud@icloud.com>
2021-05-26 01:27:21 +0300

    add: tests

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,11 @@
-import input from 'input'
-import { TelegramClient } from 'telegram'
-import { StringSession } from 'telegram/sessions'
-import { config } from './config'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-const express = require('express')
-const app = express()
-const port = 9000
-
-app.listen(port, async () => {
-  const client = new TelegramClient(
-    new StringSession(''),
-    config.appId,
-    config.appApiHash,
-    {
-      connectionRetries: 3,
-    }
-  )
-
-  await client.start({
-    phoneNumber: async () => await input.text('number ?'),
-    password: async () => await input.text('password?'),
-    phoneCode: async () => await input.text('Code ?'),
-    onError: (err) => console.log(err),
-  })
-
-  console.log('You should now be connected.')
-  console.log(client.session.save())
-  await client.sendMessage('me', { message: 'Hello!' })
-})
+import { TelegramParser } from './parser/telegram'
+;(async () => {
+  const telegramParser = new TelegramParser('myfavoritejumoreski')
+  await telegramParser.authenticate()
+  const jokes = await telegramParser.loadJokes()
+  console.log(jokes)
+  await telegramParser.close()
+})()
