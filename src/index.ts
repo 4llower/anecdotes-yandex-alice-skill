e75cc68ef42aec98038d07aae18bc68668a960c1
4llower <95350098@study.bsuir.by>
2021-05-26 00:46:14 +0300

    add: publish workflow

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,12 @@
-import input from 'input'
-import { TelegramClient } from 'telegram'
-import { StringSession } from 'telegram/sessions'
-import { config } from './config'
+import express from 'express'
 
-const express = require('express')
 const app = express()
 const port = 9000
 
+app.get('/', (_req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(port, async () => {
-  const client = new TelegramClient(
-    new StringSession(''),
-    config.appId,
-    config.appApiHash,
-    {
-      connectionRetries: 3,
-    }
-  )
-
-  await client.start({
-    phoneNumber: async () => await input.text('number ?'),
-    password: async () => await input.text('password?'),
-    phoneCode: async () => await input.text('Code ?'),
-    onError: (err) => console.log(err),
-  })
-
-  console.log('You should now be connected.')
-  console.log(client.session.save())
-  await client.sendMessage('me', { message: 'Hello!' })
+  console.log('listening on port')
 })
