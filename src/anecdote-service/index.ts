db0a102e43f23b1b847137972963179920cbe7d4
4llower <95350098@study.bsuir.by>
2021-06-04 00:00:22 +0300

    fix: perfomance

--- src/anecdote-service/index.ts
+++ src/anecdote-service/index.ts
@@ -3,14 +3,18 @@ import { FirebaseService } from '../utils'
 
 export class AnecdoteService implements IAnecdoteService {
   firebaseService
+  anecdotes
   constructor() {
     this.firebaseService = new FirebaseService()
   }
   async getRandomAnecdote() {
-    const anecdotes = await this.firebaseService.getAll()
-
+    if (!this.anecdotes) {
+      this.anecdotes = await this.firebaseService.getAll()
+    }
     return (
-      await anecdotes[Math.floor(Math.random() * (anecdotes.length - 1))].get()
+      await this.anecdotes[
+        Math.floor(Math.random() * (this.anecdotes.length - 1))
+      ].get()
     ).data().message
   }
 }
